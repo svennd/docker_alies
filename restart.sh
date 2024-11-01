9098d388e9ba9814418cf33189bdf8aa0517998b
svennd <svenn.dhert@uantwerpen.be>
2024-11-01 12:03:51 +0100

    database

--- restart.sh
+++ restart.sh
@@ -2,6 +2,10 @@
 rm -rf src && mkdir src 
 git clone --depth 1 https://github.com/svennd/alies ./src
 
+# copy config
+cp config/alies/config.php ./src/application/config/
+cp config/alies/database.php ./src/application/config/
+
 # update
 git pull
 
